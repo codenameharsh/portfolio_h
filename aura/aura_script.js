3cb2e556184aed8c9b47a8e83ea40573ce13c297
codenameharsh <harshitha167@gmail.com>
2026-01-29 13:08:09 -0600

    aura progress bar

--- aura/aura_script.js
+++ aura/aura_script.js
@@ -5,7 +5,6 @@ window.onscroll = function() {
 updateProgressBar();
 };
 
-```
 function updateProgressBar() {
     const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -20,7 +19,6 @@ function updateProgressBar() {
     }
 }
 
-```
 
 });
 
